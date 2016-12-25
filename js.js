e38e5b21742ca5996e20118e56d535d5a4351898
boyden <lee787359142@gmail.com>
2016-12-25 17:01:17 +0800

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-function color() {
-	var color_change = document.getElementById("color");
-	var red = document.getElementById('red_button');
-	var green = document.getElementById('green_button');
-	var blue = document.getElementById('blue_button');
-	var color = "#"+red.value+green.value+blue.value;
-	color_change.style.background = color;
-}
